ae0d2e16a82da637b6c51ef0ae905182577d84b7
Maxim Colls <collsmaxim@gmail.com>
2013-05-23 12:50:55 +0200

    Fixed syntax issues in data.js

--- test/data.js
+++ test/data.js
@@ -1,16 +1,18 @@
 module.exports = (function () {
-  var _ = require('underscore');
-  var Backbone = require('backbone');
+  var _ = require('underscore')
+    , Backbone = require('backbone')
+    , Models = {}
+    , Collections = {}
+    , users, tasks, projects;
+
   require('../backbone.rel');
 
-  var users, tasks, projects;
-
-  var Models = {
+  Models = {
     Task: Backbone.Model.extend({
       belongsTo: function () {
         return {
-          user: users,
-          project: function (task) {
+          user: users
+        , project: function (task) {
             return task.rel('user.project');
           }
         };
@@ -20,8 +22,8 @@ module.exports = (function () {
     User: Backbone.Model.extend({
       hasMany: function () {
         return {
-          tasks: {collection: tasks, id: 'user_id'},
-          owned_projects: {collection: projects, id: 'owner_id'}
+          tasks: {collection: tasks, id: 'user_id'}
+        , owned_projects: {collection: projects, id: 'owner_id'}
         };
       },
       belongsTo: function () {
@@ -38,23 +40,23 @@ module.exports = (function () {
             var owner_id = project.get('owner_id');
             return _.isNumber(owner_id) && users.get(owner_id);
           }
-        }
+        };
       },
       hasMany: function () {
         return {
           users: {collection: users, id: 'project_id'},
-          tasks: {
-            collection: tasks, filter: function (task) {
-              return task.rel('project') ? task.rel('project').id === this.id : null;
+          tasks: {collection: tasks, filter: function (task) {
+              return task.rel('project')
+                ? task.rel('project').id === this.id
+                : null;
             }
           }
-        }
+        };
       }
     })
   };
 
-
-  var Collections = {
+  Collections = {
     Users: Backbone.Collection.extend({
       model: Models.User
     }),
@@ -68,43 +70,32 @@ module.exports = (function () {
     })
   };
 
-
   function instance() {
-    projects = new Collections.Projects([{
-      id: 0, owner_id: 0
-    }, {
-      id: 1, owner_id: 0
-    }]);
+    projects = new Collections.Projects([
+      {id: 0, owner_id: 0}
+    , {id: 1, owner_id: 0}
+    ]);
 
-;
+    users = new Collections.Users([
+      {id: 0, project_id: 0}
+    , {id: 1, project_id: 1}
+    , {id: 2 }
+    ]);
 
-    users = new Collections.Users([{
-      id: 0, project_id: 0
-    }, {
-      id: 1, project_id: 1
-    }, {
-      id: 2
-    }]);
-
-    tasks = new Collections.Tasks([{
-      id: 0
-    }, {
-      id: 1, user_id: 1
-    }, {
-      id: 2, user_id: 0
-    }, {
-      id: 3, user_id: 1
-    }, {
-      id: 4, user_id: 0
-    }, {
-      id: 5, user_id: 1
-    }]);
+    tasks = new Collections.Tasks([
+      {id: 0}
+    , {id: 1, user_id: 1}
+    , {id: 2, user_id: 0}
+    , {id: 3, user_id: 1}
+    , {id: 4, user_id: 0}
+    , {id: 5, user_id: 1}
+    ]);
 
     return {
       projects: projects,
       users: users,
       tasks: tasks
-    }
+    };
   }
 
   return {
